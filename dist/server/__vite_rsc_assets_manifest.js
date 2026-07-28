b1031bc99a68cd769d5e78a85433694570a5fa25
333dfgfgf <giftedu76@pen.go.kr>
2026-07-28 16:43:45 +0900

    v2
    글자 수정해야함

---
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,47 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-CYtJiOWp.js\")",
+  "clientReferenceDeps": {
+    "6efdf509a785": {
+      "js": [
+        "/assets/page-CEP6hstU.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js",
+        "/assets/index-CYtJiOWp.js"
+      ],
+      "css": []
+    },
+    "8f7ba5393f33": {
+      "js": [
+        "/assets/index-CYtJiOWp.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js"
+      ],
+      "css": []
+    },
+    "ee6cb3d92c61": {
+      "js": [
+        "/assets/layout-segment-context-Cn-d_Zx2.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-CYtJiOWp.js",
+        "/assets/framework-CXnKph_e.js"
+      ],
+      "css": []
+    },
+    "1e6222746775": {
+      "js": [
+        "/assets/index-CYtJiOWp.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-nCNQIKS7.css"
+      ]
+    }
+  }
+}
